af8b1a89c3be5782b680b88371685272a78a5759
Tomas Guzma <T_Guzma@utb.cz>
2022-01-11 18:24:30 +0100

    removed white screen after splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,25 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'nutritionApp',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 5000,
+      launchAutoHide: true,
+      androidScaleType: "CENTER_CROP",
+      androidSplashResourceName: "splash",
+      splashFullScreen: false,
+      splashImmersive: false
+    },
+    android: {
+      allowMixedContent: true
+    }
+  },
 };
 
 export default config;
